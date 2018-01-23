7239e17973f89a667f9ada67636213d6b72fff59
flymanzx <891110465@qq.com>
2018-01-23 14:41:05 +0800

    ceshi

---
+++ cocoaposdtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoaposdtest
+//
+//  Created by 壹千米壹千米 on 2018/1/23.
+//  Copyright © 2018年 name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoaposdtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoaposdtest
+//
+//  Created by 壹千米壹千米 on 2018/1/23.
+//  Copyright © 2018年 name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
